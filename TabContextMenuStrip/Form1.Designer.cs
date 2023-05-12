31d46924f1247b8ad7984b70585c9020f25f72f6
Kulpot <kulpotwahu@gmail.com>
2023-05-12 16:28:52 +0800

    Add project files.

---
+++ TabContextMenuStrip/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace TabContextMenuStrip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.superTabControl1 = new TabContextMenuStrip.SuperTabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.superTabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // superTabControl1
+            // 
+            this.superTabControl1.Controls.Add(this.tabPage1);
+            this.superTabControl1.Controls.Add(this.tabPage2);
+            this.superTabControl1.Location = new System.Drawing.Point(0, 0);
+            this.superTabControl1.Name = "superTabControl1";
+            this.superTabControl1.SelectedIndex = 0;
+            this.superTabControl1.Size = new System.Drawing.Size(485, 352);
+            this.superTabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(477, 326);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 364);
+            this.Controls.Add(this.superTabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.superTabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SuperTabControl superTabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ TabContextMenuStrip/SuperTabControl.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TabContextMenuStrip
+{
+    class SuperTabControl : TabControl
+    {
+        private Point _lastClickPos;
+        private ContextMenuStrip _CMS;
+
+        public SuperTabControl()
+        {
+            _CMS = GetCMS();
+        }
+
+        private ContextMenuStrip GetCMS()
+        {
+            ContextMenuStrip CMS = new ContextMenuStrip();
+            CMS.Items.Add("Close", null, new EventHandler(Item_Clicked));
+            return CMS;
+        }
+
+        private void Item_Clicked(object sender, EventArgs e)
+        {
+            for (int i = 0; i < this.TabCount; i++)
+            {
+                Rectangle rect = this.GetTabRect(i);
+
+                //if (rect.Contains(this.PointToClient(Cursor.Position)))
+                if(rect.Contains(this.PointToClient(_lastClickPos)))
+                {
+                    this.TabPages.RemoveAt(i);
+                }
+            }
+        }
+
+            protected override void OnMouseClick(MouseEventArgs e)
+        {
+            base.OnMouseClick(e);
+
+            if(e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                _lastClickPos = Cursor.Position;
+                _CMS.Show(Cursor.Position);
+            }
+        }
+    }
+}
